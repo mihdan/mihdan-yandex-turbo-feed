709e7498b7eecb9bbf4b59f3ad8d7d42d6a13c7f
mihdan <mikhail@kobzarev.com>
2018-07-11 22:33:25 +0300

    Добавил реальный идентификатор плагина для бота Яндекса

--- templates/feed.php
+++ templates/feed.php
@@ -12,7 +12,7 @@ echo '<?xml version="1.0" encoding="' . get_option( 'blog_charset' ) . '"?' . '>
 		<link><?php bloginfo_rss( 'url' ); ?></link>
 		<description><?php bloginfo_rss( 'description' ); ?></description>
 		<language><?php echo substr( get_bloginfo_rss( 'language' ), 0, strpos( get_bloginfo_rss( 'language' ), '-' ) ); ?></language>
-		<turbo:cms_plugin>mihdan_yandex_turbo_feed</turbo:cms_plugin>
+		<turbo:cms_plugin>7391CC2B1408947EFD5084459F5BD0CA</turbo:cms_plugin>
 		<?php do_action( 'rss2_head' ); ?>
 		<?php while ( have_posts() ) : the_post(); ?>
 			<item turbo="true">
