deccd8c02cd5a15c4ad977a092406c5ead752b98
Mikhail Kobzarev <mihdan@yandex.ru>
2019-09-24 22:28:55 +0300

    Полный перезд нотификаций на WPTRT

--- includes/class-notifications.php
+++ includes/class-notifications.php
@@ -1,9 +1,9 @@
 <?php
 /**
- *
+ * @package mihdan-yandex-turbo-feed
  */
-namespace Mihdan_Yandex_Turbo_Feed;
-
+namespace Mihdan\YandexTurboFeed;
+use WPTRT\AdminNotices\Notices;
 /**
  * Class Notifications
  *
@@ -14,64 +14,31 @@ class Notifications {
 	/**
 	 * @var Settings
 	 */
-	private $redux;
+	private $settings;
 
 	/**
-	 * @var boolean
+	 * @var Notices
 	 */
-	private $is_show;
+	private $notices;
 
-	public function __construct( $redux ) {
+	public function __construct( $settings ) {
 
-		$this->redux = $redux;
+		$this->settings = $settings;
+		$this->notices  = new Notices();
 
-		$this->hooks();
-	}
-	public function hooks() {
-		add_action( 'admin_notices', array( $this, 'stars' ) );
-		add_action( 'wp_ajax_mihdan_yandex_turbo_feed_hide_notice', array( $this, 'hide_notice' ) );
-	}
-	public function is_show() {
-		if ( null === $this->is_show ) {
-			$time = $this->redux->get_option( 'rate_time' );
-			if ( ! $time ) {
-				$this->redux->set_option( 'rate_time', time() + DAY_IN_SECONDS );
-				$this->is_show = false;
-			} else {
-				$this->is_show = time() > $time;
-			}
-		}
+		$template  = '<p>';
+		$template .= __( 'Hello!', 'mihdan-yandex-turbo-feed' );
+		$template .= '<br />';
+		$template .= __( 'We are very pleased that you by now have been using the <strong>Mihdan Yandex Turbo Feed</strong> plugin a few days. Please <a href="" target="_blank">rate plugin</a>. It will help us a lot.', 'mihdan-yandex-turbo-feed' );
+		$template .= '</p>';
 
-		return $this->is_show;
-	}
-
-	public function stars() {
-		if ( ! $this->is_show() ) {
-			return;
-		}
-		?>
-		<div class="notice notice-info">
-			<p><?php _e( 'Hello!<br />We are very pleased that you by now have been using the <b>Mihdan Yandex Turbo Feed</b> plugin a few days.<br />Please rate plugin. It will help us a lot.', 'mihdan-yandex-turbo-feed' ); ?></p>
-			<p>
-				<a target="_blank" class="button button-primary js-mytf-hide-notice" data-time="<?php echo esc_attr( MONTH_IN_SECONDS ); ?>" href="<?php echo esc_url( self::URL ); ?>"><?php _e( 'Rate the plugin', 'mihdan-yandex-turbo-feed' ); ?></a>
-				<button class="button button-secondary js-mytf-hide-notice" data-time="<?php echo esc_attr( WEEK_IN_SECONDS ); ?>" type="button"><?php _e( 'Remind later', 'mihdan-yandex-turbo-feed' ); ?></button>
-				<button class="button button-secondary js-mytf-hide-notice" data-time="<?php echo esc_attr( YEAR_IN_SECONDS ); ?>" type="button"><?php _e( 'Don\'t show again', 'mihdan-yandex-turbo-feed' ); ?></button>
-			</p>
-			<p><b><?php _e( 'Thank you very much!', 'mihdan-yandex-turbo-feed' ); ?></b></p>
-		</div>
-		<?php
-	}
-
-	public function hide_notice() {
-		$time = $_POST['time'];
-
-		$this->redux->set_option( 'rate_time', time() + $time );
-
-		wp_send_json_success(
-			array(
-				't' => $time,
-			)
+		$this->notices->add(
+			MIHDAN_YANDEX_TURBO_FEED_SLUG,
+			false,
+			$template
 		);
+
+		$this->notices->boot();
 	}
 }
 
