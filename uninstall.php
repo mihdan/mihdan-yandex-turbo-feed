e6c6e3450f09252058ac63531cef6f669cc9ac44
Mikhail Kobzarev <mihdan@yandex.ru>
2020-02-19 18:22:47 +0300

    Delete plugin posts on uninstall

--- uninstall.php
+++ uninstall.php
@@ -7,9 +7,26 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
 
-// Удалить все настройки плагина.
+// Remove all plugin options.
 delete_option( 'mihdan_yandex_turbo_feed' );
 delete_option( 'mihdan_yandex_turbo_feed-transients' );
 delete_option( 'mihdan_yandex_turbo_feed_version' );
+
+delete_metadata( 'user', 0, 'mihdan_yandex_turbo_feed_review_dismissed', '', true );
+
+// Delete all posts.
+$args = [
+	'post_type'        => 'mihdan_yandex_turbo',
+	'numberposts'      => -1,
+	'post_status'      => 'any',
+	'suppress_filters' => true,
+	'fields'           => 'ids',
+];
+
+$feeds = get_posts( $args );
+
+foreach ( $feeds as $feed_id ) {
+	wp_delete_post( $feed_id, true );
+}
 
 // eof;
