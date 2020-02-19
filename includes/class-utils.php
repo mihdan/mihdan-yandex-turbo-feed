cef789b5e0acd18f58e785eba052e55f6b972e48
Mikhail Kobzarev <mihdan@yandex.ru>
2020-02-19 18:55:40 +0300

    Add method `get_post_type` & `get_slug` to `Utils`

--- includes/class-utils.php
+++ includes/class-utils.php
@@ -18,4 +18,22 @@ class Utils {
 	public function get_site_domain() {
 		return parse_url( get_bloginfo_rss( 'url' ), PHP_URL_HOST );
 	}
+
+	/**
+	 * Get plugin slug.
+	 *
+	 * @return string
+	 */
+	public function get_slug() {
+		return MIHDAN_YANDEX_TURBO_FEED_SLUG;
+	}
+
+	/**
+	 * Get post type slug.
+	 *
+	 * @return string
+	 */
+	public function get_post_type() {
+		return substr( $this->get_slug(), 0, 19 );
+	}
 }
